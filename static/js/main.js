01d63c9ae421eb5182d54e2e61e1ce9d9b755aaa
David Piccolella <davepiccolella@gmail.com>
2016-07-19 20:41:15 -0400

    starting functions

--- static/js/main.js
+++ static/js/main.js
@@ -6,23 +6,16 @@ var player_total = 0;
 var split_total_1 = 0;
 var split_total_2 = 0;
 var split_alive = true;
-
 var hit_choice = false;
 var stay_choice = false;
 var double_choice = false;
 var split_choice = false;
-
 var wins = 0;
 var losses = 0;
 var pushes = 0;
-
 var player_blackjack_flag = false;
-
-
 var split_ace_count_1 = 0;
 var split_ace_count_2 = 0;
-
-
 var ace_count = 0;
 var dealer_total = 0;
 var dealer_card_count = 0;
@@ -30,7 +23,17 @@ var dealer_ace_count = 0;
 var player_card_count = 0;
 var player_ace_count = 0;
 var num_hands = 0;
-
+var player_ace_flag=false;
+var dealer_card_list = ['x'];
+var dealer_total_list = ['x'];
+var dealer_count_list = ['x'];
+var player_card_list = ['x'];
+var player_total_list = ['x'];
+var player_count_list = ['x'];
+var split_flag = false;
+var dealer_ace_flag = false;
+var player_ace_flag = false;
+var split_flag = false;
 
 
 localStorage.setItem("total", $('#money_total').text());
@@ -191,43 +194,79 @@ var first_dict = {"A,8,2": "s"	, "A,8,3": "s"	, "A,8,4": "s"	, "A,8,5": "s"	, "A
 	, "9,3": "d"	, "9,4": "d"	, "9,5": "d"	, "9,6": "d"}
 
 
-	
+	function reset(){
+
+		$('#wager').prop('disabled', true);
+
+			hit_choice = false;
+			stay_choice= false;
+			double_choice = false;
+			split_choice = false;
+			$('#dealer_total').text("");
+			$('#hit').css("display", "none");
+			$('#stay').css("display", "none");
+			$('#stay').text("STAY");
+			$('.player_div').css("display", "none");
+			$('.dealer_div').css("display", "none");
+			$('.banner').css("display", "none");
+			$('.split_banner_left').css("display", "none");
+			$('.split_banner_right').css("display", "none");
+			$('#player_total').text("");
+			$('.card_back').css("display", "none");
+			$('#reload').css("display", "none");
+			$('#double_down').css("display", "none");
+			$('#split').css("display", "none");
+			$('#split_hit_1').css("display", "none");
+			$('#split_stay_1').css("display", "none");
+			$('#split_double_down_1').css("display", "none");
+			$('#split_hit_2').css("display", "none");
+			$('#split_stay_2').css("display", "none");
+			$('#split_double_down_2').css("display", "none");
+			$('#hit').css("background-color", "black");
+			$('#stay').css("background-color", "black");
+			$('#double_down').css("background-color", "black");
+			$('#split').css("background-color", "black");
+			if($('#split_player_total_1').is(':visible')){
+				$('#player_total').css("display", "unset");
+			}
+			$('#split_player_total_1').css("display", "none");
+			$('#split_player_total_2').css("display", "none");
+						
+			player_blackjack_flag=false;
+			card_count = 0;
+			random_number= 0;
+			player_total = 0;
+			ace_count = 0;
+			dealer_total = 0;
+			dealer_card_count = 0;
+			split_player_total_1=0;
+			split_player_total_2=0;
+			split_ace_count_1=0;
+			split_ace_count_2=0;
+			split_card_count_1=1;
+			split_card_count_2=1;
+			dealer_ace_count = 0;
+			player_card_count = 0;
+			player_ace_count = 0;
+			player_total = 0;
+			player_ace_flag=false;
+			dealer_card_list = ['x'];
+			dealer_total_list = ['x'];
+			dealer_count_list = ['x'];
+			player_card_list = ['x'];
+			player_total_list = ['x'];
+			player_count_list = ['x'];
+			split_flag = false;
 
 
 
+	};
 
-
-	$(document).ready(function(){
-
-
-		if (wins===0 && losses===0 && pushes===0){
-
-			var result = prompt("Enter your money total");
-			$('#money_total').text(result);
-			localStorage.setItem("total", $('#money_total').text());
-
-
-		}
-
-		$('#start').click(function(){
+	function start(){
 
 					
 			$('#start').css("display", "none");
 			$('#wager').prop('disabled', true);
-
-			var dealer_card_list = ['x'];
-			var dealer_total_list = ['x'];
-			var dealer_count_list = ['x'];
-
-			var player_card_list = ['x'];
-			var player_total_list = ['x'];
-			var player_count_list = ['x'];
-
-			var dealer_ace_flag = false;
-			var player_ace_flag = false;
-
-			var split_flag = false;
-
 
 
 			$('#money_total').text(localStorage.getItem("total"));
@@ -524,10 +563,6 @@ var first_dict = {"A,8,2": "s"	, "A,8,3": "s"	, "A,8,4": "s"	, "A,8,5": "s"	, "A
 
 						}
 
-						
-
-
-
 					}
 
 				}
@@ -535,6 +570,24 @@ var first_dict = {"A,8,2": "s"	, "A,8,3": "s"	, "A,8,4": "s"	, "A,8,5": "s"	, "A
 				}; } (i), 1500*i);
 
 			}
+
+	};
+
+	$(document).ready(function(){
+
+
+		if (wins===0 && losses===0 && pushes===0){
+
+			var result = prompt("Enter your money total");
+			$('#money_total').text(result);
+			localStorage.setItem("total", $('#money_total').text());
+
+
+		}
+
+		$('#start').click(function(){
+
+			start();
 
 		});
 
@@ -850,282 +903,8 @@ var first_dict = {"A,8,2": "s"	, "A,8,3": "s"	, "A,8,4": "s"	, "A,8,5": "s"	, "A
 		$('#reload').click(function(){
 
 
-			$('#wager').prop('disabled', true);
-
-			hit_choice = false;
-			stay_choice= false;
-			double_choice = false;
-			split_choice = false;
-			$('#dealer_total').text("");
-			$('#hit').css("display", "none");
-			$('#stay').css("display", "none");
-			$('#stay').text("STAY");
-			$('.player_div').css("display", "none");
-			$('.dealer_div').css("display", "none");
-			$('.banner').css("display", "none");
-			$('.split_banner_left').css("display", "none");
-			$('.split_banner_right').css("display", "none");
-			$('#player_total').text("");
-			$('.card_back').css("display", "none");
-			$('#reload').css("display", "none");
-			$('#double_down').css("display", "none");
-			$('#split').css("display", "none");
-			$('#split_hit_1').css("display", "none");
-			$('#split_stay_1').css("display", "none");
-			$('#split_double_down_1').css("display", "none");
-			$('#split_hit_2').css("display", "none");
-			$('#split_stay_2').css("display", "none");
-			$('#split_double_down_2').css("display", "none");
-			$('#hit').css("background-color", "black");
-			$('#stay').css("background-color", "black");
-			$('#double_down').css("background-color", "black");
-			$('#split').css("background-color", "black");
-			if($('#split_player_total_1').is(':visible')){
-				$('#player_total').css("display", "unset");
-			}
-			$('#split_player_total_1').css("display", "none");
-			$('#split_player_total_2').css("display", "none");
-						
-			player_blackjack_flag=false;
-			card_count = 0;
-			random_number= 0;
-			player_total = 0;
-			ace_count = 0;
-			dealer_total = 0;
-			dealer_card_count = 0;
-			split_player_total_1=0;
-			split_player_total_2=0;
-			split_ace_count_1=0;
-			split_ace_count_2=0;
-			split_card_count_1=1;
-			split_card_count_2=1;
-			dealer_ace_count = 0;
-			player_card_count = 0;
-			player_ace_count = 0;
-			player_total = 0;
-			var player_ace_flag=false;
-			var dealer_card_list = ['x'];
-			var dealer_total_list = ['x'];
-			var dealer_count_list = ['x'];
-			var player_card_list = ['x'];
-			var player_total_list = ['x'];
-			var player_count_list = ['x'];
-			var split_flag = false;
-			
-
-
-			for(var i = 0; i<2;i++){
-
-				random_number = Math.floor(Math.random()*(52));
-				dealer_card_list.push(card_array[random_number]);
-
-				if (card_dict[card_array[random_number]] === 11){
-
-					dealer_ace_count++;
-
-				}
-
-
-				dealer_total = dealer_total + card_dict[card_array[random_number]];
-
-				if(dealer_total>21 && dealer_ace_count>0){
-
-					dealer_total=dealer_total-10;
-					dealer_ace_count=dealer_ace_count-1;
-					dealer_ace_flag = true;
-					
-				}
-
-				dealer_card_count++;
-
-				dealer_total_list.push(dealer_total);
-				dealer_count_list.push(dealer_card_count);
-
-			}
-	
-			for (var i = 1; i<dealer_total_list.length; i++){
-				setTimeout(function(x) { return function() { 
-					$('body').append('<div class=\"dealer_div\" style =\"left:' + parseInt(460+(dealer_count_list[x]-1)*45,10)+ 'px\"><img src=\"static/img/' 
-					+ dealer_card_list[x] +'\"/></div>');
-					if (x===2){
-						$('body').append('<div class = \"card_back\"><img src=\"static/img/card_back.jpg\"/></div>');
-					}
-				
-					
-				if (x===1){
-						$('#dealer_total').text(dealer_total_list[1]);
-						$('#dealer_popup').text("+" + card_dict[dealer_card_list[x]]);
-						$('#dealer_popup').fadeIn("fast");
-						$('#dealer_popup').fadeOut("fast");
-				}
-				}; } (i), 1000*i);
-			}
-			
-			for(var i = 0; i<2;i++){
-				random_number = Math.floor(Math.random()*(52));
-				player_card_list.push(card_array[random_number]);
-				if (card_dict[card_array[random_number]] === 11){
-					player_ace_count++;
-				}
-				player_total = player_total + card_dict[card_array[random_number]];
-				if(player_total>21 && player_ace_count>0){
-					player_total=player_total-10;
-					player_ace_count=player_ace_count-1;
-					player_ace_flag = true;
-					
-				};
-				player_card_count++;
-				player_total_list.push(player_total);
-				player_count_list.push(player_card_count);
-			}
-			if (player_card_list[1].split('_')[0] === player_card_list[2].split('_')[0]){
-				split_flag = true;
-			}
-			for (var i = 1; i<player_total_list.length; i++){
-				setTimeout(function(x) { return function() { 
-				
-					$('body').append('<div class=\"player_div\" style =\"left:' + parseInt(400+(dealer_count_list[x]-2)*45,10)+ 'px\"><img src=\"static/img/' 
-					+ player_card_list[x] +'\"/></div>');
-
-
-					$('#player_total').text(player_total_list[x]);
-
-					if(x===2 && player_ace_flag){
-
-						$('#player_popup').text("+1");
-
-					} else
-					{
-
-						$('#player_popup').text("+" + card_dict[player_card_list[x]]);
-
-					}
-					
-					$('#player_popup').fadeIn("fast");
-					$('#player_popup').fadeOut("fast");
-
-					if(x===2){
-
-
-						if (player_total === 21){
-
-							$('#stay').css("display","unset");
-							$('#stay').text("Cont");
-							player_blackjack_flag=true;
-
-						}
-
-						else{
-
-						$('#hit').css("display", "unset");
-						$('#stay').css("display", "unset");
-						$('#double_down').css("display", "unset");
-						$('#hit').css("left", "375px");
-						$('#stay').css("left", "500px");
-						$('#double_down').css("left", "625px");
-
-						/*if (split_flag){*/
-							$('#split').css("display", "unset");
-						/*}*/
-
-						var decision_key_1 = "";
-						switch(player_card_list[1].split('_')[0]){
-							case "jack":
-								decision_key_1 = decision_key_1 + "J";
-								break;
-							case "queen":
-								decision_key_1 = decision_key_1 + "Q";
-								break;
-							case "king":
-								decision_key_1 = decision_key_1 + "K";
-								break;
-							case "ace":
-								decision_key_1 = decision_key_1 + "A";
-								break;
-							default:
-								decision_key_1 = decision_key_1 + player_card_list[1].split('_')[0]
-						}
-						decision_key_1 = decision_key_1 + ",";
-						switch(player_card_list[2].split('_')[0]){
-							case "jack":
-								decision_key_1 = decision_key_1 + "J";
-								break;
-							case "queen":
-								decision_key_1 = decision_key_1 + "Q";
-								break;
-							case "king":
-								decision_key_1 = decision_key_1 + "K";
-								break;
-							case "ace":
-								decision_key_1 = decision_key_1 + "A";
-								break;
-							default:
-								decision_key_1 = decision_key_1 + player_card_list[2].split('_')[0]
-						}
-						decision_key_1 = decision_key_1 + ",";
-						switch(dealer_card_list[1].split('_')[0]){
-							case "jack":
-								decision_key_1 = decision_key_1 + "J";
-								break;
-							case "queen":
-								decision_key_1 = decision_key_1 + "Q";
-								break;
-							case "king":
-								decision_key_1 = decision_key_1 + "K";
-								break;
-							case "ace":
-								decision_key_1 = decision_key_1 + "A";
-								break;
-							default:
-								decision_key_1 = decision_key_1 + dealer_card_list[1].split('_')[0]
-						}
-						if (first_dict[decision_key_1]===undefined){
-							if (number_dict[player_total_list[2]+","+dealer_total_list[1]] === undefined){
-								hit_choice = true; 
-								$('#hit').css("background-color", "red");
-							} else if (number_dict[player_total_list[2]+","+dealer_total_list[1]] === "s")
-							{
-								stay_choice = true;
-								$('#stay').css("background-color", "red");
-							}
-							else if (number_dict[player_total_list[2]+","+dealer_total_list[1]] === "d")
-							{
-								double_choice = true;
-								$('#double_down').css("background-color", "red");
-							}
-							else if (number_dict[player_total_list[2]+","+dealer_total_list[1]] === "sp")
-							{
-							
-								split_choice = true;
-								$('#split').css("background-color", "red");
-							}
-							
-						}
-						else if (first_dict[decision_key_1]==="s"){
-							stay_choice=true;
-							$('#stay').css("background-color", "red");
-						}
-						else if (first_dict[decision_key_1]==="d"){
-							double_choice=true;
-							$('#double_down').css("background-color", "red");
-						} 
-						else if (first_dict[decision_key_1]==="sp"){
-							split_choice=true;
-							$('#split').css("background-color", "red");
-						}
-
-
-					}
-
-					$('#player_total').text(player_total_list[x]);
-					
-					
-			}
-				}; } (i), 1500*i);
-
-			
-
-			} 
+			reset();
+			start();
 		
 		});
 
