539ddc84e23991d2976494d16481af6e31c8fab3
David Piccolella <davepiccolella@gmail.com>
2016-07-26 21:45:41 -0400

    Fixing split

--- static/js/main.js
+++ static/js/main.js
@@ -1178,8 +1178,60 @@ function start(){
 		$('body').append('<div class=\"player_div\" style =\"left:250px\"><img src=\"static/img/' 
 					+ source_array[0].split('/')[source_array[0].split('/').length-1] +'\"/></div>');
 
+		random_number = Math.floor(Math.random()*(52));
+
+				$('body').append('<div class=\"player_div\" style =\"left:' + parseInt(200+(split_card_count_1+1)*45,10)+ 'px\"><img src=\"static/img/' 
+					+ card_array[random_number] +'\"/></div>');
+
+				if (card_dict[card_array[random_number]] === 11){
+
+					split_ace_count_1++;
+
+				};
+
+				split_total_1 = split_total_1 + card_dict[card_array[random_number]];
+
+				$('#split_player_total_1').text(split_total_1);
+
+				
+
+					$('#split_popup_1').text("+" + card_dict[card_array[random_number]]);
+				
+				
+				$('#split_popup_1').fadeIn("fast");
+				$('#split_popup_1').fadeOut("fast"); 
+				
+				split_card_count_1 = split_card_count_1 + 1;
+
 		$('body').append('<div class=\"player_div\" style =\"left:750px\"><img src=\"static/img/' 
 					+ source_array[1].split('/')[source_array[1].split('/').length-1] +'\"/></div>');
+
+		random_number = Math.floor(Math.random()*(52));
+
+		$('body').append('<div class=\"player_div\" style =\"left:' + parseInt(700+(split_card_count_2+1)*45,10)+ 'px\"><img src=\"static/img/' 
+					+ card_array[random_number] +'\"/></div>');
+
+		if (card_dict[card_array[random_number]] === 11){
+
+					split_ace_count_2++;
+
+				};
+
+				split_total_2 = split_total_2 + card_dict[card_array[random_number]];
+
+				$('#split_player_total_2').text(split_total_2);
+
+				
+
+					$('#split_popup_2').text("+" + card_dict[card_array[random_number]]);
+				
+				
+				$('#split_popup_2').fadeIn("fast");
+				$('#split_popup_2').fadeOut("fast"); 
+				
+				split_card_count_2 = split_card_count_2 + 1;
+
+
 
 		$('#split_1_hit').css("display", "unset");
 		$('#split_1_stay').css("display", "unset");
@@ -1211,7 +1263,7 @@ function start(){
 
 				if (card_dict[card_array[random_number]] === 11){
 
-					player_ace_count++;
+					split_ace_count_1++;
 
 				};
 
